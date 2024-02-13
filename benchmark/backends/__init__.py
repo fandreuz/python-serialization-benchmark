1c050a2506e042021deb8511386a8c8bfce8c68b
fAndreuzzi <andreuzzi.francesco@gmail.com>
2024-02-14 00:13:29 +0100

    rapid json

--- benchmark/backends/__init__.py
+++ benchmark/backends/__init__.py
@@ -1,4 +1,5 @@
 from .bjson import JsonBackend  # noqa: F401
 from .borjson import OrjsonBackend  # noqa: F401
+from .brapidjson import RapidjsonBackend  # noqa: F401
 from .bpickle4 import Pickle4Backend  # noqa: F401
 from .bpickle5 import Pickle5Backend  # noqa: F401
---
+++ benchmark/backends/brapidjson.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from dataclasses import asdict
+from typing import Type
+
+from rapidjson import dumps, loads
+
+from benchmark.base import Backend
+from benchmark.cases import NumericArrayObject, TextObject
+
+from .utils import as_serializable_dict, handle_numpy_fields
+
+
+class RapidjsonBackend(Backend[bytes]):
+    @classmethod
+    def serialize(cls, obj: TextObject | NumericArrayObject) -> bytes:
+        dc = asdict(obj)
+        return dumps(as_serializable_dict(dc), default=str)
+
+    @classmethod
+    def deserialize(
+        cls, serialized: bytes, target_type: Type[TextObject | NumericArrayObject]
+    ) -> TextObject | NumericArrayObject:
+        dc = loads(serialized)
+        return target_type(**handle_numpy_fields(dc))
