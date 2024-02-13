be03d203146be30ab189f246fa6674b36a2035ab
fAndreuzzi <andreuzzi.francesco@gmail.com>
2024-02-14 00:10:46 +0100

    use common utils

--- benchmark/backends/bjson.py
+++ benchmark/backends/bjson.py
@@ -4,31 +4,21 @@ from dataclasses import asdict
 from json import dumps, loads
 from typing import Type
 
-import numpy as np
-
 from benchmark.base import Backend
 from benchmark.cases import NumericArrayObject, TextObject
+
+from .utils import as_serializable_dict, handle_numpy_fields
 
 
 class JsonBackend(Backend[str]):
     @classmethod
     def serialize(cls, obj: TextObject | NumericArrayObject) -> str:
         dc = asdict(obj)
-        serializable_dc = {}
-        for key, value in dc.items():
-            if isinstance(value, np.ndarray):
-                value = value.tolist()
-            serializable_dc[key] = value
-        return dumps(serializable_dc, default=str)
+        return dumps(as_serializable_dict(dc), default=str)
 
     @classmethod
     def deserialize(
         cls, serialized: str, target_type: Type[TextObject | NumericArrayObject]
     ) -> TextObject | NumericArrayObject:
         dc = loads(serialized)
-        deserialized_dc = {}
-        for key, value in dc.items():
-            if isinstance(value, (list, tuple)):
-                value = np.array(value)
-            deserialized_dc[key] = value
-        return target_type(**deserialized_dc)
+        return target_type(**handle_numpy_fields(dc))
