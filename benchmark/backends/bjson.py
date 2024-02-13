e6feaeaf75a7141ede5f0ccf095dd97b79423a87
fAndreuzzi <andreuzzi.francesco@gmail.com>
2024-02-13 23:35:25 +0100

    serialize/deserialize numpy

--- benchmark/backends/bjson.py
+++ benchmark/backends/bjson.py
@@ -4,6 +4,8 @@ from dataclasses import asdict
 from json import dumps, loads
 from typing import Type
 
+import numpy as np
+
 from benchmark.base import Backend
 from benchmark.cases import NumericArrayObject, TextObject
 
@@ -11,10 +13,22 @@ from benchmark.cases import NumericArrayObject, TextObject
 class JsonBackend(Backend[str]):
     @classmethod
     def serialize(cls, obj: TextObject | NumericArrayObject) -> str:
-        return dumps(asdict(obj), default=str)
+        dc = asdict(obj)
+        serializable_dc = {}
+        for key, value in dc.items():
+            if isinstance(value, np.ndarray):
+                value = value.tolist()
+            serializable_dc[key] = value
+        return dumps(serializable_dc, default=str)
 
     @classmethod
     def deserialize(
         cls, serialized: str, target_type: Type[TextObject | NumericArrayObject]
     ) -> TextObject | NumericArrayObject:
-        return target_type(**loads(serialized))
+        dc = loads(serialized)
+        deserialized_dc = {}
+        for key, value in dc.items():
+            if isinstance(value, (list, tuple)):
+                value = np.array(value)
+            deserialized_dc[key] = value
+        return target_type(**deserialized_dc)
