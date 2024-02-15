a91e492e0f149dc882ba784389d7fa8d58f2bf2d
fAndreuzzi <andreuzzi.francesco@gmail.com>
2024-02-15 23:45:42 +0100

    force dtype

--- benchmark/run/numeric_benchmark.py
+++ benchmark/run/numeric_benchmark.py
@@ -5,10 +5,10 @@ from benchmark.run.base import pretty_print, run_backends
 
 
 def generate_obj(size):
-    x = np.linspace(0, 1, size)
-    y = np.linspace(-1, 1, size)
-    z = np.linspace(0, 1, size)
-    values = np.random.rand(size)
+    x = np.linspace(0, 1, size, dtype=np.float64)
+    y = np.linspace(-1, 1, size, dtype=np.float64)
+    z = np.linspace(0, 1, size, dtype=np.float64)
+    values = np.random.rand(size).astype(dtype=np.float64)
     info = Information(
         "Numeric benchmark", "Data for numeric benchmark", 0, "fandreuz@cern.ch"
     )
